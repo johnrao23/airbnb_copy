766d4995c914c8ac25081c437c7eacbf6a6e029a
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-02-27 14:54:40 -0500

    Updating main UI
    Preliminary changes made to app.jsx

--- fairbnb/src/App.jsx
+++ fairbnb/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import main from './assets/tropical_main.png'
 import './App.css'
+import { Image } from 'react-bootstrap';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,25 +10,23 @@ function App() {
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Image
+        src={main}
+        alt="main page background"
+      ></Image>
       </div>
-      <h1>Vite + React</h1>
-      <h1>Fairbnb App</h1>
+      <h1>Fairbnb</h1>
+      <h2>A place to get last minute travel deals at a fair price!</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Click to find your next adventure.
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p className="extra-info">
+        Search for the location you want to travel to
       </p>
     </div>
   )
