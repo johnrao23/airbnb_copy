3abad33c94d913641caad2f707a31513c7a68faf
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-03-01 15:39:24 -0500

    Dependency changes
    Bootstrap --> tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+    <div>
       {/* <div>
       <Image
         src={main}
@@ -14,7 +14,7 @@ function App() {
       </div> */}
       <h1>Fairbnb</h1>
       <h2>A place to get last minute travel deals at a fair price!</h2>
-      <div className="card">
+      <div>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -22,7 +22,7 @@ function App() {
           Click to find your next adventure.
         </p>
       </div>
-      <p className="extra-info">
+      <p>
         Search for the location you want to travel to
       </p>
     </div>
