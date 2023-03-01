2b95e83bd36d33be5e13637d12c9e6e7d820a6e3
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-03-01 15:33:23 -0500

    Installed tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import main from './assets/tropical_main.png'
-import './App.css'
-import { Image } from 'react-bootstrap';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
+      {/* <div>
       <Image
         src={main}
         alt="main page background"
       ></Image>
-      </div>
+      </div> */}
       <h1>Fairbnb</h1>
       <h2>A place to get last minute travel deals at a fair price!</h2>
       <div className="card">
