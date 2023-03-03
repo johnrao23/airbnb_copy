7f9bf717ca85d4b0e2a8d609b6698bdffbba4ba6
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-03-03 11:02:29 -0500

    Updated firebaseExample.jsx

--- src/components/firebaseExample.jsx
+++ src/components/firebaseExample.jsx
@@ -1,22 +1,43 @@
 import React, {useState, useEffect} from 'react'
 import * as firebase from 'firebase/app';
-import db from '../../firebase';
+import { getFirestore,collection, getDocs, addDoc } from 'firebase/firestore';
+// import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
 
-// Initialize Firebase with your configuration details
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAD3mvL_xqfGDCbwC4M50FjrglGvBdZWxA",
-//   authDomain: "fairbnb-5b50b.firebaseapp.com",
-//   projectId: "fairbnb-5b50b",
-//   storageBucket: "fairbnb-5b50b.appspot.com",
-//   messagingSenderId: "686702915592",
-//   appId: "1:686702915592:web:544e15a1cb72a2130be1bd",
-// };
+const user = {
+  name: "John",
+  email: "john@example.com",
+  age: 30,
+};
 
-// firebase.initializeApp(firebaseConfig);
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAD3mvL_xqfGDCbwC4M50FjrglGvBdZWxA",
+  authDomain: "fairbnb-5b50b.firebaseapp.com",
+  projectId: "fairbnb-5b50b",
+  storageBucket: "fairbnb-5b50b.appspot.com",
+  messagingSenderId: "686702915592",
+  appId: "1:686702915592:web:544e15a1cb72a2130be1bd",
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const usersCollection = collection(db, "users");
+
+// Add a document to the "users" collection
+async function addUser(user) {
+  try {
+    const docRef = await addDoc(usersCollection, user);
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+}
+
+addUser(user);
 
 
-// Reference to the Firestore database
-// const db = firebase.firestore();
 
 const Firebase = () => {
   const [data, setData] = useState([]);
@@ -24,6 +45,16 @@ const Firebase = () => {
   useEffect(() => {
     // Fetch some data from Firestore
     console.log("database: ", db)
+    // Get a reference to the "users" collection
+const usersRef = collection(db, 'users');
+
+// Fetch all the documents in the "users" collection
+    getDocs(usersRef).then((querySnapshot) => {
+      setData(querySnapshot)
+  // querySnapshot.forEach((doc) => {
+  //   console.log(doc.id, '=>', doc.data());
+  // });
+});
     // db.collection('users')
     //   .get()
     //   .then((querySnapshot) => {
