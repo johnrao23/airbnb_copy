6b56ec8556676a009f9c67fa0e84637b6128bdef
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-03-27 13:55:09 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import LocationSearch from './components/locationSearch';
 import Firebase from './components/firebaseExample';
+import BeachImg from './assets/tropical_main.png';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,7 +9,7 @@ function App() {
   
   return (
     <div>
-      <h1 >Fairbnb</h1>
+      <h1>Fairbnb</h1>
       <h2>A place to get last minute travel deals at a fair price!</h2>
       <div>
         <button onClick={() => setCount((count) => count + 1)}>
@@ -23,6 +24,7 @@ function App() {
       <p>
         Search for the location you want to travel to
       </p>
+      <img src={BeachImg} alt="Beach" />
     </div>
   )
 }
