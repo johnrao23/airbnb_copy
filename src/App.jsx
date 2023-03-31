519c130c0a7041df23694c127c5578b381c09045
0xFloyd <32312309+0xFloyd@users.noreply.github.com>
2023-03-31 10:22:01 -0400

    remove git modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,22 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import LocationSearch from './components/locationSearch';
 import Firebase from './components/firebaseExample';
 import BeachImg from './assets/tropical_main.png';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  
   return (
     <div>
       <h1>Fairbnb</h1>
       <h2>A place to get last minute travel deals at a fair price!</h2>
       <div>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <Firebase/>
+        <Firebase />
         <LocationSearch />
-        <p className='text-red-500'>
-          Click to find your next adventure.
-        </p>
+        <p className="text-red-500">Click to find your next adventure.</p>
       </div>
-      <p>
-        Search for the location you want to travel to
-      </p>
+      <p>Search for the location you want to travel to</p>
       <img src={BeachImg} alt="Beach" />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/firebaseExample.jsx
+++ src/components/firebaseExample.jsx
@@ -1,62 +1,60 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react';
 import * as firebase from 'firebase/app';
-import { getFirestore,collection, getDocs, addDoc } from 'firebase/firestore';
+import { getFirestore, collection, getDocs, addDoc } from 'firebase/firestore';
 // import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
 
 const user = {
-  name: "John",
-  email: "john@example.com",
+  name: 'John',
+  email: 'john@example.com',
   age: 30,
 };
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAD3mvL_xqfGDCbwC4M50FjrglGvBdZWxA",
-  authDomain: "fairbnb-5b50b.firebaseapp.com",
-  projectId: "fairbnb-5b50b",
-  storageBucket: "fairbnb-5b50b.appspot.com",
-  messagingSenderId: "686702915592",
-  appId: "1:686702915592:web:544e15a1cb72a2130be1bd",
+  apiKey: 'AIzaSyAD3mvL_xqfGDCbwC4M50FjrglGvBdZWxA',
+  authDomain: 'fairbnb-5b50b.firebaseapp.com',
+  projectId: 'fairbnb-5b50b',
+  storageBucket: 'fairbnb-5b50b.appspot.com',
+  messagingSenderId: '686702915592',
+  appId: '1:686702915592:web:544e15a1cb72a2130be1bd',
 };
 
 // Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-const usersCollection = collection(db, "users");
+const usersCollection = collection(db, 'users');
 
 // Add a document to the "users" collection
 async function addUser(user) {
   try {
     const docRef = await addDoc(usersCollection, user);
-    console.log("Document written with ID: ", docRef.id);
+    console.log('Document written with ID: ', docRef.id);
   } catch (e) {
-    console.error("Error adding document: ", e);
+    console.error('Error adding document: ', e);
   }
 }
 
-addUser(user);
-
-
+// addUser(user);
 
 const Firebase = () => {
-  const [data, setData] = useState([]);
+  const [users, setUsers] = useState([]);
 
   useEffect(() => {
     // Fetch some data from Firestore
-   
-    // Get a reference to the "users" collection
-const usersRef = collection(db, 'users');
 
-// Fetch all the documents in the "users" collection
+    // Get a reference to the "users" collection
+    const usersRef = collection(db, 'users');
+
+    // Fetch all the documents in the "users" collection
     getDocs(usersRef).then((querySnapshot) => {
-    let huh =[]
-  querySnapshot.forEach((doc) => {
-    console.log(doc.id, '=>', doc.data());
-    huh.push({id: doc.id, data: doc.data()})
-  });
-      setData(huh)
-});
+      let tempUsers = [];
+      querySnapshot.forEach((doc) => {
+        // console.log(doc.id, '=>', doc.data());
+        tempUsers.push({ id: doc.id, data: doc.data() });
+      });
+      setUsers(tempUsers);
+    });
     // db.collection('users')
     //   .get()
     //   .then((querySnapshot) => {
@@ -68,19 +66,17 @@ const usersRef = collection(db, 'users');
     //   });
   }, []);
 
-  console.log("data:", data)
-
   return (
-
     <>
-      <p className='text-red-500'>firebase</p>
-      {data?.length && <ul>
-        {data.map((item) => (
-          <li key={item.id}>{item.data.email}</li>
-        ))}
-      </ul>}
-      </>
-  )
-}
+      {users?.length && (
+        <ul>
+          {users.map((item) => (
+            <li key={item.id}>{item.data.email}</li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
+};
 
-export default Firebase
+export default Firebase;
