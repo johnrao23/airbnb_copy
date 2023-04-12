66dd4ac10717dc59d35a42df2f65ca404f1f9788
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-04-12 16:37:47 -0400

    Configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import LocationSearch from "./components/locationSearch";
 // @ts-ignore
 import BeachImg from "./assets/tropical_main.png";
+import SignUpPage from "./Backend/Signup/SignUpPage";
 
 function App() {
   return (
@@ -8,6 +10,13 @@ function App() {
       <h1>Fairbnb</h1>
       <h2>A place to get last minute travel deals at a fair price!</h2>
       <div>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/signup" exact>
+              <SignUpPage />
+            </Route>
+          </Switch>
+        </BrowserRouter>
         <LocationSearch />
         <p className="text-red-500">Click to find your next adventure.</p>
       </div>
