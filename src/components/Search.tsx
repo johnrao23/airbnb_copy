03f1aa54eccb8e01bc571daa2e9ef130f28a158d
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-07-03 18:17:39 -0400

    Updating state

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -4,14 +4,16 @@ import { useAuthStore } from "../Backend/store/store"
 
 const Search = () => {
     const user = useAuthStore((state) => state.user);
-    const { searchResults } = useAuthStore();
+    const searchResults = useAuthStore((state) => state.searchResults);
+
+    console.log(searchResults)
 
     return (
         <div>
             <NavBar />
             <h1>Get ready to live it up, {user?.email}</h1>
             {Array.isArray(searchResults.results) && searchResults.results.map((result) => (
-                  <div key={result.id}>
+                <div key={result.id}>
                   <p>{result.url}</p>
                   <img src={result.images[0]} alt="Result" />
                 </div>
