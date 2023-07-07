3e9de9c9c3afb25cdb5706c64e2f9f45c445f4e0
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-07-07 18:06:50 -0400

    Update Search.tsx

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -15,13 +15,13 @@ const Search = () => {
     return (
         <div>
             <NavBar />
-            <h1 className="text-center text-2xl font-bold my-4">Get ready to live it up for less $, {user?.email}</h1>
+            <h1 className="text-center text-2xl font-bold my-4">Get ready to live it up for less, {user?.email}</h1>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
             {Array.isArray(searchResults.results.results) && searchResults.results.results.map((result) => (
                 <div key={result.id} className="border border-gray-300 rounded-lg p-4 m-2 flex flex-col items-center justify-between space-y-4 h-full">
-                  <p className="overflow-hidden break-all mb-2 text-center">{result.url}</p>
-                  <img className="rounded-lg mb-2" src={result.images[0]} alt="Result" />
-                  <p className="mb-2">${result.price.total}</p>
+                  <p className="overflow-hidden break-all mb-2 text-center h-12">{result.url}</p>
+                  <img className="rounded-lg mb-2 h-48 w-full object-cover" src={result.images[0]} alt="Result" />
+                  <p className="mb-2 text-green-500">${result.price.total}</p>
                   <button className="bg-blue-500 text-white rounded p-2 hover:bg-blue-700 mt-auto" onClick={() => handleSubmit()}>Reserve</button>
                 </div>
             ))}
