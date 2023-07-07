49fbc003a2c3a9fc9c0d7957abe2b20841187aa7
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-07-07 17:57:08 -0400

    Reservation Page state updates

--- src/components/ReservationPage.tsx
+++ src/components/ReservationPage.tsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { useAuthStore } from "../Backend/store/store"
 
 const Reservation = () => {
+    const user = useAuthStore((state) => state.user);
+
     return <h1>Landing Page from Search Result Choices to Book Reservation</h1>
 }
 
