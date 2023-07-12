eee4ae227ac71f2f2543de6091c8d5bb7b6ad666
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-07-12 16:20:09 -0400

    Updated styling
    JIT mode for tailwinds

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: "jit",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
