fd593ff09cd65977a5a710b68685c75c2b6e6d88
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-07-13 22:41:45 -0400

    Create UserProfile.tsx

---
+++ src/components/UserProfile.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import { useAuthStore } from "Backend/store/store";
+
+const Profile = () => {
+    const user = useAuthStore((state) => state.user);
+
+    return (
+        <div>
+            <h1>This will display info about the user and current user state/transactions</h1>
+            
+            <h1>{user?.email}</h1>
+        </div>
+    )
+}
+
+export default Profile
