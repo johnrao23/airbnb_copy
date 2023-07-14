c4b66ed891292d789c37de914918d708301b7325
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-07-14 17:14:40 -0400

    Update UserProfile.tsx

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -12,7 +12,7 @@ const Profile = () => {
         <div>
             <NavBar />
             <h1>This will display info about the user and current user state/transactions</h1>
-            <h1>{user.name}</h1>
+            <h1>{user?.name}</h1>
             <h1>{user?.email}</h1>
         </div>
     )
