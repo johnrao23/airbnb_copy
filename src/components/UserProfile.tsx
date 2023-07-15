457f05aef0d1d7c875328e81e03251fd5bc70ca4
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-07-15 15:48:34 -0400

    Update UserProfile.tsx

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -5,16 +5,41 @@ import { useAuthStore } from "../Backend/store/store";
 const Profile = () => {
 
     const user = useAuthStore((state) => state.user);
-    const [userinfo, setUserInfo] = useState("")
+    const setUser = useAuthStore((state) => state.setUser);
+    const [userinfo, setUserInfo] = useState(user?.name || "")
+    const [submitted, setSubmitted] = useState(false)
 
-    // Set up field to store profile info and save it to Application state
+    const handleNameChange = (event) => {
+        setUserInfo(event.target.value);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // Update the user with the new name
+        setUser({ ...user, name: userinfo });
+        // Indicate that the form has been submitted
+        setSubmitted(true);
+    }
 
     return (
         <div>
             <NavBar />
-            <h1>This will display info about the user and current user state/transactions</h1>
-            <h1>{user?.name}</h1>
-            <h1>{user?.email}</h1>
+            <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+                <h1 className="text-2xl text-gray-800 mb-4">Let us get to know you a little bit, {user?.email}</h1>
+                {!submitted ? (
+                    <form onSubmit={handleSubmit} className="w-full max-w-md mx-auto p-6 space-y-2 bg-white rounded-xl shadow-md">
+                        <div>
+                            <label className="block text-gray-700 text-lg font-semibold mb-2">Name:</label>
+                            <input className="form-input mt-1 block w-full px-4 py-2 rounded-lg border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500" type="text" value={userinfo} onChange={handleNameChange} />
+                        </div>
+                        <div>
+                            <input type="submit" value="Submit" className="w-full flex justify-center py-2 px-4 border border-transparent rounded-lg shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" />
+                        </div>
+                    </form>
+                ) : (
+                    <h2 className="text-2xl text-gray-800 mb-4">Thank you, {user?.name}</h2>
+                )}
+            </div>
         </div>
     )
 }
