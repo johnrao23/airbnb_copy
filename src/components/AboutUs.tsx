067422182517de5f0bc3811defdb35d12c24b1f8
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-07-16 15:49:12 -0400

    Importing image for About Us page

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -1,13 +1,23 @@
 import React from "react";
 import { useAuthStore } from "../Backend/store/store";
+import nycImg from "../assets/nycImg.png";
+import NavBar from "./NavBar";
 
 const About = () => {
     const user = useAuthStore((state) => state.user);
 
     return (
-        <div>
-            <h1>Thanks for asking about us, {user?.email}!</h1>
-            <p>Here at Fairbnb, we want to provide you with once in a lifetime opportunites for a much better price!</p>
+        <div className="min-h-screen flex flex-col">
+            <div 
+                className="relative flex-grow bg-cover bg-no-repeat pb-20" 
+                style={{ 
+                    backgroundImage: `url(${nycImg})`,
+                }}
+            >
+                <NavBar />
+                <h1>Thanks for asking about us, {user?.email}!</h1>
+                <p>Here at Fairbnb, we want to provide you with once in a lifetime opportunites for a much better price!</p>
+            </div>
         </div>
     )
 }
