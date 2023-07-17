90ff92c2fd8e13c246a83653ffc120647f9d2c0e
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-07-17 14:41:56 -0400

    Update SignInPage.tsx

--- src/Backend/UserActions/Signin/SignInPage.tsx
+++ src/Backend/UserActions/Signin/SignInPage.tsx
@@ -168,24 +168,25 @@ export const SignIn = () => {
                 <div className="mt-6 grid grid-cols-3 gap-3">
                   <div>
                     <a
-                      href="#"
-                      className="inline-flex w-full justify-center rounded-md bg-white px-4 py-2 text-gray-500 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0"
+                        href="#"
+                        className="inline-flex w-full justify-center rounded-md bg-white px-4 py-2 text-gray-500 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0"
                     >
-                      <span className="sr-only">Sign in with Facebook</span>
-                      <svg
-                        className="h-5 w-5"
-                        aria-hidden="true"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M20 10c0-5.523-4.477-10-10-10S0 4.477 0 10c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V10h2.54V7.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V10h2.773l-.443 2.89h-2.33v6.988C16.343 19.128 20 14.991 20 10z"
-                          clipRule="evenodd"
-                        />
-                      </svg>
+                        <span className="sr-only">Sign in with Google</span>
+                        <svg
+                            className="h-5 w-5"
+                            aria-hidden="true"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                        >
+                            <path
+                                fillRule="evenodd"
+                                d="M10 9.978V14H15.196C14.667 16.328 12.935 18 10.5 18 7.468 18 5 15.532 5 12.5S7.468 7 10.5 7C12.041 7 13.345 7.66 14.11 8.775L16.896 6.082C15.363 4.57 13.296 3.5 10.5 3.5C6.364 3.5 3 6.864 3 11S6.364 18.5 10.5 18.5C15.196 18.5 18.5 14.358 18.5 11.153 18.5 10.384 18.364 9.978 18 9.36H10Z"
+                                clipRule="evenodd"
+                            />
+                        </svg>
                     </a>
                   </div>
+
 
                   <div>
                     <a
