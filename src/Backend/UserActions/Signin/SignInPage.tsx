5d61239ca522612815795553292d7f5bfea61568
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-07-18 23:04:23 -0400

    Update SignInPage.tsx

--- src/Backend/UserActions/Signin/SignInPage.tsx
+++ src/Backend/UserActions/Signin/SignInPage.tsx
@@ -274,7 +274,7 @@ export const SignIn = () => {
 
 
                   <div>
-                    <a
+                    <button
                       className="inline-flex w-full justify-center rounded-md bg-white px-4 py-2 text-gray-500 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0"
                       onClick={() => handleTwitterSignIn()}
                     >
@@ -287,7 +287,7 @@ export const SignIn = () => {
                       >
                         <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
                       </svg>
-                    </a>
+                    </button>
                   </div>
 
 
