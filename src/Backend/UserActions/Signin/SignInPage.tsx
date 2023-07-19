70a459909ec5cbfafc2ae84512bf44730ab0ceaa
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-07-18 23:04:02 -0400

    Update SignInPage.tsx

--- src/Backend/UserActions/Signin/SignInPage.tsx
+++ src/Backend/UserActions/Signin/SignInPage.tsx
@@ -275,8 +275,8 @@ export const SignIn = () => {
 
                   <div>
                     <a
-                      href="#"
                       className="inline-flex w-full justify-center rounded-md bg-white px-4 py-2 text-gray-500 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0"
+                      onClick={() => handleTwitterSignIn()}
                     >
                       <span className="sr-only">Sign in with Twitter</span>
                       <svg
