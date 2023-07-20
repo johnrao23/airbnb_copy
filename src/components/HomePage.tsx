3fe1e7512c479219bba89a9bd5ddb18af7e7b685
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-07-19 22:59:50 -0400

    Update HomePage.tsx

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -47,11 +47,11 @@ const HomePage: React.FC = () => {
       >
         <div className="absolute inset-0 bg-black opacity-50" />
         <NavBar />
-  
+    
         <div className="relative flex flex-col items-center h-full text-white transform translate-y-[255%]">
           <h1>Welcome, {user?.email}</h1>
           <p className="text-green-500 text-2xl mb-4">Click Search to find your next adventure.</p>
-  
+    
           <form onSubmit={handleSearch} className="mb-4">
             <p className="text-lg mb-2">Search for the location you want to travel to:</p>
             <div className="flex">
@@ -59,12 +59,24 @@ const HomePage: React.FC = () => {
                 type="search"
                 placeholder="Search here"
                 className="border border-gray-400 rounded-l-lg px-3 py-2 w-64 focus:outline-none text-black"
-                onChange={handleChange}
+                onChange={e => setSearchInput(e.target.value)}
                 value={searchInput}
+              />
+              <input
+                type="date"
+                className="border border-gray-400 px-3 py-2 w-64 focus:outline-none text-black ml-2"
+                onChange={e => setCheckInDate(e.target.value)}
+                value={checkInDate}
+              />
+              <input
+                type="date"
+                className="border border-gray-400 px-3 py-2 w-64 focus:outline-none text-black ml-2"
+                onChange={e => setCheckOutDate(e.target.value)}
+                value={checkOutDate}
               />
               <button
                 type="submit"
-                className="bg-blue-500 text-white px-4 py-2 rounded-r-lg ml-1 focus:outline-none hover:bg-blue-600"
+                className="bg-blue-500 text-white px-4 py-2 rounded-r-lg ml-2 focus:outline-none hover:bg-blue-600"
               >
                 Search
               </button>
@@ -73,8 +85,7 @@ const HomePage: React.FC = () => {
         </div>
       </div>
     </div>
-  );  
-};
+  );
 
 export default HomePage;
 
