e247d4b833f92bab1ef6f9642977933a958fd2f3
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-07-21 13:01:46 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { auth } from "./Backend/Firebase/firebaseConfig";
+import { getRedirectResult } from 'firebase/auth';
+import { useAuthStore } from "./Backend/store/store";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import Search from "./components/Search"
@@ -11,6 +14,21 @@ import SignIn from "./Backend/UserActions/Signin/SignInPage";
 import ProtectedRoute from "./components/ProtectedRoute.jsx";
 
 function App() {
+  useEffect(() => {
+    getRedirectResult(auth)
+      .then((result) => {
+        if (result.user) {
+          useAuthStore.setState({
+            user: { id: result.user.uid, email: result.user.email, name: result.user.displayName },
+            isSignedIn: true,
+          });
+        }
+      })
+      .catch((error) => {
+        console.error("An error occurred during sign-in", error);
+      });
+  }, []);
+
   return (
     <div>
       <BrowserRouter>
@@ -72,3 +90,4 @@ function App() {
 }
 
 export default App;
+
