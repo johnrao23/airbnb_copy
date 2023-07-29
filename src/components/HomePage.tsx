da13c01be0b9bdd6c69bf574025decb6bc853168
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-07-28 23:27:28 -0400

    Update HomePage.tsx

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -45,38 +45,39 @@ const HomePage: React.FC = () => {
         <div className="absolute w-full top-[55%] flex flex-col items-center text-white pb-20">
           <h1>Welcome, {user?.email}</h1>
           <p className="text-green-500 text-2xl mb-4">Click Search to find your next adventure.</p>
-          <form onSubmit={handleSearch} className="mb-4">
-            <p className="text-lg mb-2">Search for the location you want to travel to:</p>
-            <div className="flex flex-col items-center md:flex-row md:items-start">
-              <input
-                type="search"
-                placeholder="Search here"
-                className="border border-gray-400 rounded-lg px-3 py-2 w-full md:w-[196px] focus:outline-none text-black mb-4 md:mb-0"
-                onChange={e => setSearchInput(e.target.value)}
-                value={searchInput}
-              />
-              <div className="flex flex-col md:flex-row md:w-[404px] md:space-x-2 space-y-2 md:space-y-0">
-                <input
-                  type="date"
-                  className="border border-gray-400 px-3 py-2 w-full focus:outline-none text-black"
-                  onChange={e => setCheckInDate(e.target.value)}
-                  value={checkInDate}
-                />
-                <input
-                  type="date"
-                  className="border border-gray-400 px-3 py-2 w-full focus:outline-none text-black"
-                  onChange={e => setCheckOutDate(e.target.value)}
-                  value={checkOutDate}
-                />
-                <button
-                  type="submit"
-                  className="bg-blue-500 text-white px-4 py-2 rounded-lg w-full md:w-auto focus:outline-none hover:bg-blue-600"
-                >
-                  Search
-                </button>
-              </div>
-            </div>
-          </form>
+<form onSubmit={handleSearch} className="mb-4">
+  <p className="text-lg mb-2">Search for the location you want to travel to:</p>
+  <div className="flex flex-col items-center space-y-4 md:space-y-0 md:flex-row md:items-start">
+    <input
+      type="search"
+      placeholder="Search here"
+      className="border border-gray-400 rounded-lg px-3 py-2 w-full md:w-[480px] focus:outline-none text-black mb-4 md:mb-0"
+      onChange={e => setSearchInput(e.target.value)}
+      value={searchInput}
+    />
+    <div className="flex flex-col space-y-2 md:space-y-0 md:flex-row md:w-[480px] md:space-x-2">
+      <input
+        type="date"
+        className="border border-gray-400 px-3 py-2 w-full focus:outline-none text-black"
+        onChange={e => setCheckInDate(e.target.value)}
+        value={checkInDate}
+      />
+      <input
+        type="date"
+        className="border border-gray-400 px-3 py-2 w-full focus:outline-none text-black"
+        onChange={e => setCheckOutDate(e.target.value)}
+        value={checkOutDate}
+      />
+      <button
+        type="submit"
+        className="bg-blue-500 text-white px-4 py-2 rounded-lg w-full md:w-auto focus:outline-none hover:bg-blue-600"
+      >
+        Search
+      </button>
+    </div>
+  </div>
+</form>
+
         </div>
       </div>
     </div>
