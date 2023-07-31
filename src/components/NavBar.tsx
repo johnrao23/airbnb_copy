b9851f9e7945ebf3bb13ed7e395d3a08a7c325ba
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-07-31 11:45:01 -0400

    Update NavBar.tsx

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,29 +1,24 @@
-import React from "react";
-import { Fragment } from "react";
-import SignOut from "../Backend/UserActions/Signout";
+import React, { Fragment } from "react";
+import { Link, useLocation } from "react-router-dom";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
-import { Link } from "react-router-dom";
-import ProfileImg from "../assets/profileicon.png"
-
-interface NavigationItem {
-  name: string;
-  href: string;
-  current: boolean;
-}
-
-const navigation: NavigationItem[] = [
-  { name: "Home", href: "/home", current: true },
-  { name: "Search", href: "/search", current: false },
-  { name: "About Us", href: "/about", current: false },
-  { name: "Contact", href: "/contactus", current: false },
-];
+import SignOut from "../Backend/UserActions/Signout";
+import ProfileImg from "../assets/profileicon.png";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
 export default function NavBar() {
+  const location = useLocation();
+
+  const navigation = [
+    { name: "Home", href: "/home" },
+    { name: "Search", href: "/search" },
+    { name: "About Us", href: "/about" },
+    { name: "Contact", href: "/contactus" },
+  ];
+
   return (
     <Disclosure as="nav" className="bg-gray-800">
       {({ open }) => (
@@ -31,7 +26,7 @@ export default function NavBar() {
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                {/* Mobile menu button*/}
+                {/* Mobile menu button */}
                 <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
@@ -61,12 +56,12 @@ export default function NavBar() {
                         key={item.name}
                         to={item.href}
                         className={classNames(
-                          item.current
+                          location.pathname === item.href
                             ? "bg-gray-900 text-white"
                             : "text-gray-300 hover:bg-gray-700 hover:text-white",
                           "rounded-md px-3 py-2 text-sm font-medium"
                         )}
-                        aria-current={item.current ? "page" : undefined}
+                        aria-current={location.pathname === item.href ? "page" : undefined}
                       >
                         {item.name}
                       </Link>
@@ -150,23 +145,23 @@ export default function NavBar() {
             </div>
           </div>
 
+          {/* Mobile Menu */}
           <Disclosure.Panel className="sm:hidden">
-            <div className="space-y-1 px-2 pb-3 pt-2">
+            <div className="px-2 pt-2 pb-3 space-y-1">
               {navigation.map((item) => (
-                <Disclosure.Button
+                <Link
                   key={item.name}
-                  as={Link}
                   to={item.href}
                   className={classNames(
-                    item.current
+                    location.pathname === item.href
                       ? "bg-gray-900 text-white"
                       : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                    "block rounded-md px-3 py-2 text-base font-medium"
+                    "rounded-md px-3 py-2 text-sm font-medium"
                   )}
-                  aria-current={item.current ? "page" : undefined}
+                  aria-current={location.pathname === item.href ? "page" : undefined}
                 >
                   {item.name}
-                </Disclosure.Button>
+                </Link>
               ))}
             </div>
           </Disclosure.Panel>
