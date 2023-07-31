bb1fbebfd216744b73f5488de6682da155d05bcd
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-07-31 11:13:22 -0400

    Contact Background Img

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,11 +1,19 @@
 import React from "react";
+import CraterLake from "../assets/craterLake.png"
 
 const Contact = () => {
     return (
         <div>
+            <div 
+                className="relative flex-grow bg-cover bg-no-repeat" 
+                style={{ 
+                backgroundImage: `url(${CraterLake})`,
+                }}
+            >
             <h1>Need some help? Please feel free to reach out!</h1>
             <h1>support@sameplacebetterprice.com</h1>
             <h1>(800) 555-1234</h1>
+            </div>
         </div>
     )
 }
