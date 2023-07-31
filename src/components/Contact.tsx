d04f8cf43dd49e6ceb18560af43181bcadebd202
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-07-31 11:16:11 -0400

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,21 +1,24 @@
 import React from "react";
-import CraterLake from "../assets/craterLake.png"
+import CraterLake from "../assets/craterLake.png";
 
 const Contact = () => {
-    return (
-        <div>
-            <div 
-                className="relative flex-grow bg-cover bg-no-repeat" 
-                style={{ 
-                backgroundImage: `url(${CraterLake})`,
-                }}
-            >
-            <h1>Need some help? Please feel free to reach out!</h1>
-            <h1>support@sameplacebetterprice.com</h1>
-            <h1>(800) 555-1234</h1>
-            </div>
+  return (
+    <div className="bg-gray-100 min-h-screen flex items-center justify-center">
+      <div
+        className="relative flex-grow bg-cover bg-no-repeat bg-center p-10 rounded-lg shadow-lg"
+        style={{
+          backgroundImage: `url(${CraterLake})`,
+          width: '800px',
+        }}
+      >
+        <div className="bg-white bg-opacity-50 p-10 rounded">
+          <h1 className="text-3xl font-bold mb-4">Need some help? Please feel free to reach out!</h1>
+          <h1 className="text-xl mb-2">support@sameplacebetterprice.com</h1>
+          <h1 className="text-xl">(800) 555-1234</h1>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Contact;
