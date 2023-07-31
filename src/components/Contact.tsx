2bf75ffd42028eee5ac7bada93275fea40433214
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-07-31 11:36:51 -0400

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,22 +4,22 @@ import NavBar from "./NavBar";
 
 const Contact = () => {
   return (
-    <>
+    <div className="overflow-hidden h-screen flex flex-col">
       <NavBar />
       <div
-        className="flex items-center justify-center min-h-screen w-full bg-no-repeat bg-center"
+        className="flex items-center justify-center flex-grow w-full bg-no-repeat bg-center"
         style={{
-            backgroundImage: `url(${CraterLake})`,
-            backgroundSize: '130% 105%',
+          backgroundImage: `url(${CraterLake})`,
+          backgroundSize: '130% 105%',
         }}
-        >
+      >
         <div className="bg-white bg-opacity-60 p-10 rounded-lg shadow-lg text-center w-1/2">
-            <h1 className="text-4xl font-bold mb-4">Need some help? Please feel free to reach out!</h1>
-            <h1 className="text-2xl mb-2">support@sameplacebetterprice.com</h1>
-            <h1 className="text-2xl">(800) 555-1234</h1>
+          <h1 className="text-4xl font-bold mb-4">Need some help? Please feel free to reach out!</h1>
+          <h1 className="text-2xl mb-2">support@sameplacebetterprice.com</h1>
+          <h1 className="text-2xl">(800) 555-1234</h1>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
