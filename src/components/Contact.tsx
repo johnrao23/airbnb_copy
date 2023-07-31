47ec5d4e42b8f034518a756674883b643154ae7d
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-07-31 11:16:35 -0400

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,19 +3,16 @@ import CraterLake from "../assets/craterLake.png";
 
 const Contact = () => {
   return (
-    <div className="bg-gray-100 min-h-screen flex items-center justify-center">
-      <div
-        className="relative flex-grow bg-cover bg-no-repeat bg-center p-10 rounded-lg shadow-lg"
-        style={{
-          backgroundImage: `url(${CraterLake})`,
-          width: '800px',
-        }}
-      >
-        <div className="bg-white bg-opacity-50 p-10 rounded">
-          <h1 className="text-3xl font-bold mb-4">Need some help? Please feel free to reach out!</h1>
-          <h1 className="text-xl mb-2">support@sameplacebetterprice.com</h1>
-          <h1 className="text-xl">(800) 555-1234</h1>
-        </div>
+    <div
+      className="flex items-center justify-center min-h-screen bg-cover bg-no-repeat bg-center"
+      style={{
+        backgroundImage: `url(${CraterLake})`,
+      }}
+    >
+      <div className="bg-white bg-opacity-60 p-10 rounded-lg shadow-lg text-center w-1/2">
+        <h1 className="text-4xl font-bold mb-4">Need some help? Please feel free to reach out!</h1>
+        <h1 className="text-2xl mb-2">support@sameplacebetterprice.com</h1>
+        <h1 className="text-2xl">(800) 555-1234</h1>
       </div>
     </div>
   );
