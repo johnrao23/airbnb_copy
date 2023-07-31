4d1e1b9fe358749b4699f40d722cf81101a6c434
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-07-31 11:02:34 -0400

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,7 +3,9 @@ import React from "react";
 const Contact = () => {
     return (
         <div>
-            <h1>Contact Us Page</h1>
+            <h1>Need some help? Please feel free to reach out!</h1>
+            <h1>support@sameplacebetterprice.com</h1>
+            <h1>(800) 555-1234</h1>
         </div>
     )
 }
