6c8d84ad4a72e3d1e883807d4b0002fd18294c10
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-07-31 14:25:30 -0400

    Update Search.tsx

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import NavBar from "./NavBar";
 import { useAuthStore } from "../Backend/store/store";
 import { useNavigate } from "react-router-dom";
@@ -9,54 +9,60 @@ const Search = () => {
   const setSelectedResult = useAuthStore((state) => state.setSelectedResult);
   const navigate = useNavigate();
 
-  // State variable to track if the user has clicked without searching
   const [hasClicked, setHasClicked] = useState(false);
+
+  useEffect(() => {
+    // Set hasClicked to true as soon as the component is rendered
+    setHasClicked(true);
+  }, []);
 
   const handleSubmit = (result) => {
     setSelectedResult(result);
     return navigate("/reserve");
   };
 
-  const handleUserClick = () => {
-    // Set hasClicked to true when user clicks anywhere on the page
-    setHasClicked(true);
-  };
-
   return (
     <div
-      onClick={handleUserClick}
-      className={`${!searchResults.results.results.length && hasClicked ? "bg-gray-100" : ""
-        }`}
+      style={{
+        backgroundColor: !searchResults.results.results.length && hasClicked ? 'aqua' : 'transparent',
+        minHeight: '100vh',
+      }}
     >
       <NavBar />
       <h1 className="text-center text-2xl font-bold my-4">
         Get ready to live it up for less, {user?.email}
       </h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
-        {Array.isArray(searchResults.results.results) &&
-          searchResults.results.results.map((result) => (
-            <div
-              key={result.id}
-              className="border border-gray-300 rounded-lg p-4 m-2 flex flex-col items-center justify-between space-y-4 h-full"
-            >
-              <p className="overflow-hidden break-all mb-2 text-center h-12">
-                {result.name}
-              </p>
-              <img
-                className="rounded-lg mb-2 h-48 w-full object-cover"
-                src={result.images[0]}
-                alt="Result"
-              />
-              <p className="mb-2 text-green-500">${result.price.total}</p>
-              <button
-                className="bg-blue-500 text-white rounded p-2 hover:bg-blue-700 mt-auto"
-                onClick={() => handleSubmit(result)}
+      { !searchResults.results.results.length && hasClicked ? (
+        <div className="text-center text-xl font-semibold">
+          Come back with some vacation ideas!
+        </div>
+      ) : (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+          {Array.isArray(searchResults.results.results) &&
+            searchResults.results.results.map((result) => (
+              <div
+                key={result.id}
+                className="border border-gray-300 rounded-lg p-4 m-2 flex flex-col items-center justify-between space-y-4 h-full"
               >
-                Reserve
-              </button>
-            </div>
-          ))}
-      </div>
+                <p className="overflow-hidden break-all mb-2 text-center h-12">
+                  {result.name}
+                </p>
+                <img
+                  className="rounded-lg mb-2 h-48 w-full object-cover"
+                  src={result.images[0]}
+                  alt="Result"
+                />
+                <p className="mb-2 text-green-500">${result.price.total}</p>
+                <button
+                  className="bg-blue-500 text-white rounded p-2 hover:bg-blue-700 mt-auto"
+                  onClick={() => handleSubmit(result)}
+                >
+                  Reserve
+                </button>
+              </div>
+            ))}
+        </div>
+      )}
     </div>
   );
 };
