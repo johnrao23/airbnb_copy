bbad9ecbb6e9481204ba890b553afe0b33962255
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-08-08 14:27:57 -0400

    Update HomePage.tsx

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -82,7 +82,7 @@ const HomePage: React.FC = () => {
                 />
               </div>
               <div className="flex flex-col sm:flex-row justify-center space-y-2 sm:space-y-0">
-                <div className="flex justify-center mb-2 sm:mb-0"> {/* Added div */}
+                <div className="flex justify-center mb-2 sm:mb-0">
                   <input
                     type="date"
                     className="border border-gray-400 px-3 py-2 w-full sm:w-64 focus:outline-none text-black text-base sm:text-base"
@@ -90,7 +90,7 @@ const HomePage: React.FC = () => {
                     value={checkInDate}
                   />
                 </div>
-                <div className="flex justify-center mb-2 sm:mb-0"> {/* Added div */}
+                <div className="flex justify-center mb-2 sm:mb-0">
                   <input
                     type="date"
                     className="border border-gray-400 px-3 py-2 w-full sm:w-64 focus:outline-none text-black ml-2 sm:ml-2 text-base sm:text-base"
