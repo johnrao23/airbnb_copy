bc6fa23d98ee32a7082415a2844bca6b68c4526e
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-08-08 14:49:33 -0400

    Update HomePage.tsx

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -70,43 +70,44 @@ const HomePage: React.FC = () => {
           <h1 className="text-lg sm:text-2xl">Welcome, {user?.email}</h1>
           <p className="text-green-500 text-xl mb-2 sm:text-2xl sm:mb-4">Click Search to find your next adventure.</p>
           <form onSubmit={handleSearch} className="mb-2 sm:mb-4">
-            <p className="text-sm sm:text-lg mb-1 sm:mb-2">Search for the location you want to travel to:</p>
-            <div className="mb-2 sm:mb-4">
-              <div className="mx-auto sm:mx-0 w-full sm:w-auto"> {/* Centering on mobile */}
-                <input
-                  type="search"
-                  placeholder="Search here"
-                  className="border border-gray-400 rounded-lg px-3 py-2 w-full focus:outline-none text-black text-xs sm:text-base"
-                  onChange={e => {setSearchInput(e.target.value); setShowAlert(false);}}
-                  value={searchInput}
-                />
-              </div>
-            </div>
-            <div className="flex flex-col sm:flex-row justify-center space-y-2 sm:space-y-0">
-              <div className="flex justify-center mb-2 sm:mb-0">
-                <input
-                  type="date"
-                  className="border border-gray-400 px-3 py-2 w-full sm:w-64 focus:outline-none text-black text-base sm:text-base"
-                  onChange={e => {setCheckInDate(e.target.value); setShowAlert(false);}}
-                  value={checkInDate}
-                />
-              </div>
-              <div className="flex justify-center mb-2 sm:mb-0">
-                <input
-                  type="date"
-                  className="border border-gray-400 px-3 py-2 w-full sm:w-64 focus:outline-none text-black md:ml-2 text-base sm:text-base"
-                  onChange={e => {setCheckOutDate(e.target.value); setShowAlert(false);}}
-                  value={checkOutDate}
-                />
-              </div>
-              <button
-                type="submit"
-                className="bg-blue-500 text-white px-4 py-2 w-full sm:w-auto rounded-lg sm:rounded-r-lg ml-2 focus:outline-none hover:bg-blue-600 text-base sm:text-base mx-auto sm:mx-0"
-              >
-                Search
-              </button>
-            </div>
-          </form>
+  <p className="text-sm sm:text-lg mb-1 sm:mb-2">Search for the location you want to travel to:</p>
+  <div className="mb-2 sm:mb-4">
+    <input
+      type="search"
+      placeholder="Search here"
+      className="border border-gray-400 rounded-lg px-3 py-2 w-full focus:outline-none text-black text-xs sm:text-base"
+      onChange={e => {setSearchInput(e.target.value); setShowAlert(false);}}
+      value={searchInput}
+    />
+  </div>
+  <div className="flex flex-col sm:flex-row justify-center space-y-2 sm:space-y-0">
+    <div className="flex justify-center mb-2 sm:mb-0">
+      <input
+        type="date"
+        className="border border-gray-400 px-3 py-2 w-full sm:w-64 focus:outline-none text-black text-base sm:text-base"
+        onChange={e => {setCheckInDate(e.target.value); setShowAlert(false);}}
+        value={checkInDate}
+      />
+    </div>
+    <div className="flex justify-center mb-2 sm:mb-0">
+      <input
+        type="date"
+        className="border border-gray-400 px-3 py-2 w-full sm:w-64 focus:outline-none text-black md:ml-2 text-base sm:text-base"
+        onChange={e => {setCheckOutDate(e.target.value); setShowAlert(false);}}
+        value={checkOutDate}
+      />
+    </div>
+    <div className="flex justify-center sm:justify-start"> {/* Centering the button on mobile */}
+      <button
+        type="submit"
+        className="bg-blue-500 text-white px-4 py-2 w-full sm:w-auto rounded-lg sm:rounded-r-lg md:ml-2 focus:outline-none hover:bg-blue-600 text-base sm:text-base"
+      >
+        Search
+      </button>
+    </div>
+  </div>
+</form>
+
         </div>
       </div>
     </div>
