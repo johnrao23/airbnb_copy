ff9f4d45ad175dc355a8948c161cd745c4ee2a6d
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-08-11 14:41:07 -0400

    Update AboutUs.tsx

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -9,13 +9,13 @@ const About = () => {
 
   return (
     <div className="min-h-screen flex flex-col">
+      <NavBar />
       <div 
         className="relative flex-grow bg-cover bg-no-repeat bg-center pb-5 md:pb-20 text-center md:text-right" 
         style={{ 
           backgroundImage: `url(${nycImg})`,
         }}
       >
-        <NavBar />
         <div className="absolute inset-x-0 md:right-0 bg-blue-500 bg-opacity-90 text-white p-3 md:p-5 mx-3 md:mx-5 mt-5 text-center">
           <h1 className="text-2xl md:text-4xl">Thanks for asking about us, {user?.email}!</h1>
           <p className="text-lg md:text-xl">Here at Fairbnb, we want to provide you with once in a lifetime opportunities for a much better price!</p>
