b21d3d1e45c7971c72285280f61f43f9610e0b7f
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-08-23 13:59:06 -0400

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -8,15 +8,17 @@ const Contact = () => {
     <div className="overflow-hidden h-screen flex flex-col">
       <NavBar />
       <div
-        className="flex items-center justify-center flex-grow w-full bg-no-repeat bg-center bg-cover md:bg-150/130"
+        className="flex flex-col justify-end flex-grow w-full bg-no-repeat bg-center bg-cover md:bg-150/130 pb-10 md:pb-20"
         style={{
           backgroundImage: `url(${CraterLake})`,
         }}
       >
-        <div className="bg-white bg-opacity-60 p-5 md:p-10 rounded-lg shadow-lg text-center w-11/12 md:w-1/2">
-          <h1 className="text-2xl md:text-4xl font-bold mb-2 md:mb-4">Need some help? Please feel free to reach out!</h1>
-          <h1 className="text-xl md:text-2xl mb-1 md:mb-2">support@sameplacebetterprice.com</h1>
-          <h1 className="text-xl md:text-2xl">(800) 555-1234</h1>
+        <div className="flex justify-center">
+          <div className="bg-white bg-opacity-10 p-5 md:p-10 rounded-lg shadow-lg text-center w-11/12 md:w-1/2">
+            <h1 className="text-2xl md:text-4xl font-bold mb-2 md:mb-4">Need some help? Please feel free to reach out!</h1>
+            <h1 className="text-xl md:text-2xl mb-1 md:mb-2">support@sameplacebetterprice.com</h1>
+            <h1 className="text-xl md:text-2xl">(800) 555-1234</h1>
+          </div>
         </div>
       </div>
       <Footer />
