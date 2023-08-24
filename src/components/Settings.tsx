807b90171ca5b7b424b35d282cd03a385a0307ea
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-08-24 14:32:07 -0400

    Create Settings.tsx

---
+++ src/components/Settings.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Settings = () => {
+    return (
+        <div>
+            <h1>This is where user settings will be.</h1>
+        </div>
+    )
+}
+
+export default Settings;
