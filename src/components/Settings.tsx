3a8cd17878e4ae8ee7fde9850326e41361b16ad9
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-08-25 11:41:25 -0400

    Update Settings.tsx

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -8,9 +8,18 @@ const Settings = () => {
 
 
     return (
-        <div>
+        <div className="min-h-screen flex flex-col">
             <NavBar />
-            <h1>Hello, {user?.email}</h1>
+                <div 
+                    className="relative flex-grow bg-cover bg-no-repeat bg-center pb-5 md:pb-20 text-center md:text-right" 
+                    style={{ 
+                        backgroundImage: `url(${nycImg})`,
+                    }}
+                    >
+                    <div className="bg-blue-500 bg-opacity-90 text-white p-3 md:p-5 rounded-lg shadow-lg mx-auto md:mx-auto mt-20 text-center max-w-xl">
+                        <h1 className="text-2xl md:text-4xl">How can we help, {user?.email}?</h1>
+                    </div>
+                </div>
             <Footer />
         </div>
     )
