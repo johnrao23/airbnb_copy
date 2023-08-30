c2a4040f4366aeb91493c052ffe5773f1be07c85
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-08-30 11:34:34 -0400

    env

--- src/Backend/api/chatGPT.ts
+++ src/Backend/api/chatGPT.ts
@@ -1,13 +1,27 @@
-export async function getGPTResponse(prompt: string) {
-    const response = await fetch("/api/chat", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
+const axios = require('axios');
+
+module.exports = async (req, res) => {
+  const { prompt } = req.body;
+
+  const GPT_API_KEY = process.env.GPT_API_KEY;
+
+  try {
+    const response = await axios.post("https://api.openai.com/v1/engines/davinci-codex/completions", 
+      {
+        prompt,
+        max_tokens: 50
       },
-      body: JSON.stringify({ prompt }),
-    });
-  
-    const data = await response.json();
-    return data;
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${GPT_API_KEY}`
+        }
+      }
+    );
+
+    res.status(200).json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "An error occurred while communicating with the GPT-3 API" });
   }
-  
+};
