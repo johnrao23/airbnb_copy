3fc3f40aebe9788916ff04da3c0b98cf82dfdd9e
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-08-30 09:55:47 -0400

    chatGPT api

---
+++ src/Backend/api/chatGPT.ts
@@ -0,0 +1,13 @@
+async function getGPTResponse(prompt) {
+    const response = await fetch("/api/chat", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ prompt }),
+    });
+  
+    const data = await response.json();
+    return data;
+  }
+  
