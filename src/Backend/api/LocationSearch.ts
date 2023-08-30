e4c5b27b1b7aee3f2cbc3686f7c1c95d92294e82
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-08-30 10:26:54 -0400

    have to use vercel for env variables instead

--- src/Backend/api/LocationSearch.ts
+++ src/Backend/api/LocationSearch.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
-require("dotenv").config();
 
-const apiKey = process.env.API_KEY
-const apiHost = process.env.API_HOST;
+const API_KEY = '578c6464e2mshc3cfe32bae51a74p127e53jsnc298999d88ec';
+const API_HOST = 'airbnb13.p.rapidapi.com';
 
 export const locationSearch = async (location: string, checkin: string, checkout: string) => {
   try {
@@ -19,8 +18,8 @@ export const locationSearch = async (location: string, checkin: string, checkout
         currency: 'USD'
       },
       headers: {
-        'X-RapidAPI-Key': apiKey,
-        'X-RapidAPI-Host': apiHost
+        'X-RapidAPI-Key': API_KEY,
+        'X-RapidAPI-Host': API_HOST
       }
     });
 
