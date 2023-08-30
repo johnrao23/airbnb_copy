844999cd757b8aaa8854550cfd87ab0423dace58
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-08-30 09:59:10 -0400

    Update Settings.tsx

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useAuthStore } from "../Backend/store/store";
 import { getGPTResponse } from "../Backend/api/chatGPT";
 import NavBar from "./NavBar";
@@ -7,6 +7,24 @@ import buildingdockImg from "../assets/buildingdockImg.png"
 
 const Settings = () => {
     const user = useAuthStore((state) => state.user);
+    const [chatHistory, setChatHistory] = useState([]);
+    const [userInput, setUserInput] = useState("");
+
+    const handleQuestionSubmit = async (e) => {
+        e.preventDefault();
+    
+        // Append user question to chat history
+        setChatHistory([...chatHistory, { type: "user", text: userInput }]);
+    
+        // Fetch GPT-3 response
+        const gptResponse = await getGPTResponse(userInput);
+    
+        // Append GPT-3 response to chat history
+        setChatHistory([...chatHistory, { type: "user", text: userInput }, { type: "gpt", text: gptResponse.text }]);
+    
+        // Clear user input
+        setUserInput("");
+      };
 
 
     return (
@@ -20,6 +38,23 @@ const Settings = () => {
                     >
                     <div className="bg-gray-500 bg-opacity-30 text-white p-3 md:p-5 rounded-lg shadow-lg mx-auto md:mx-auto mt-20 text-center max-w-xl">
                         <h1 className="text-2xl md:text-4xl">How can we help, {user?.twitterUsername || user?.displayName || user?.email}?</h1>
+                    </div>
+                    <div className="chat-container">
+                        <ul>
+                        {chatHistory.map((msg, index) => (
+                            <li key={index} className={msg.type}>
+                            {msg.text}
+                            </li>
+                        ))}
+                        </ul>
+                        <form onSubmit={handleQuestionSubmit}>
+                        <input
+                            type="text"
+                            value={userInput}
+                            onChange={(e) => setUserInput(e.target.value)}
+                        />
+                        <button type="submit">Ask</button>
+                        </form>
                     </div>
                 </div>
             <Footer />
