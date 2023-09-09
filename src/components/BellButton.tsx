ce64821919762cdd25e52d8f8958f8f63e9bb2c5
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-09-09 13:10:11 -0400

    Update BellButton.tsx

--- src/components/BellButton.tsx
+++ src/components/BellButton.tsx
@@ -2,33 +2,33 @@ import React, { useState } from 'react';
 import { BellIcon } from "@heroicons/react/24/outline";
 
 function NotificationButton() {
-  const [showDropdown, setShowDropdown] = useState(false);
-  const notifications = ['Notification 1', 'Notification 2', 'Notification 3'];
-
-  return (
-    <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0 relative">
-      <button
-        type="button"
-        className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-        onClick={() => setShowDropdown(!showDropdown)}
-      >
-        <span className="sr-only">View notifications</span>
-        <BellIcon className="h-6 w-6" aria-hidden="true" />
-      </button>
-
-      {showDropdown && (
-        <div className="absolute right-0 mt-2 w-48 rounded-md shadow-lg bg-white">
-          <div className="rounded-md ring-1 ring-black ring-opacity-5">
-            {notifications.map((notif, index) => (
-              <div key={index} className="py-2 px-4 hover:bg-gray-200">
-                {notif}
-              </div>
-            ))}
+    const [showDropdown, setShowDropdown] = useState(false);
+    const notifications = ['Notifications'];
+  
+    return (
+      <div className="relative inline-block text-left">
+        <button
+          type="button"
+          className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+          onClick={() => setShowDropdown(!showDropdown)}
+        >
+          <span className="sr-only">View notifications</span>
+          <BellIcon className="h-6 w-6" aria-hidden="true" />
+        </button>
+  
+        {showDropdown && (
+          <div className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg bg-white">
+            <div className="rounded-md ring-1 ring-black ring-opacity-5">
+              {notifications.map((notif, index) => (
+                <div key={index} className="py-2 px-4 hover:bg-gray-200">
+                  {notif}
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
-      )}
-    </div>
-  );
-}
+        )}
+      </div>
+    );
+  }  
 
 export default NotificationButton;
