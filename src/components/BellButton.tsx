e45fc9d1d93fb00e7c33667746bdd89375081fb7
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-09-09 13:07:24 -0400

    Bell Button

--- src/components/BellButton.tsx
+++ src/components/BellButton.tsx
@@ -1,19 +1,19 @@
 import React, { useState } from 'react';
+import { BellIcon } from "@heroicons/react/24/outline";
 
 function NotificationButton() {
   const [showDropdown, setShowDropdown] = useState(false);
   const notifications = ['Notification 1', 'Notification 2', 'Notification 3'];
 
   return (
-    <div className="relative">
+    <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0 relative">
       <button
         type="button"
         className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
         onClick={() => setShowDropdown(!showDropdown)}
       >
         <span className="sr-only">View notifications</span>
-        {/* Replace <BellIcon /> with your icon component */}
-        <span className="h-6 w-6" aria-hidden="true">🔔</span>
+        <BellIcon className="h-6 w-6" aria-hidden="true" />
       </button>
 
       {showDropdown && (
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,7 @@
 import React, { Fragment } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
-import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import NotificationButton from "./BellButton"
 import SignOut from "../Backend/UserActions/Signout";
 import ProfileImg from "../assets/profileicon.png";
@@ -72,14 +72,15 @@ export default function NavBar() {
                 </div>
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <button
+                <NotificationButton />
+                {/* <button
                   onClick={NotificationButton()}
                   type="button"
                   className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                 >
                   <span className="sr-only">View notifications</span>
                   <BellIcon className="h-6 w-6" aria-hidden="true" />
-                </button>
+                </button> */}
 
                 {/* Profile dropdown */}
                 <Menu as="div" className="relative ml-3">
