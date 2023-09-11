728bac2daa8df0b0dd19a9024e796643c25283b4
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-09-11 13:43:35 -0400

    Update HomePage.tsx

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -56,7 +56,6 @@ const HomePage: React.FC = () => {
     setIsLoading(false);
 };
 
-
   return (
     <div className="min-h-screen flex flex-col overflow-hidden">
       <NavBar />
@@ -71,29 +70,25 @@ const HomePage: React.FC = () => {
         </div>
       )}
       <div 
-          className="relative flex-grow bg-cover bg-no-repeat" 
-          style={{ 
-              backgroundImage: `url(${beachImg})`,
-          }}
+        className="relative flex-grow bg-cover bg-no-repeat" 
+        style={{ 
+          backgroundImage: `url(${beachImg})`,
+        }}
       >
         <div className="absolute inset-0 bg-black opacity-50" />
-        <div className="absolute w-full top-[72%] transform -translate-y-1/2 flex flex-col items-center text-white px-4 md:px-0 text-xs sm:text-base bg-white bg-opacity-10 p-3 md:p-5 rounded-lg shadow-lg">
-            <h1 className="text-lg sm:text-2xl mb-2">
-                Welcome, {user?.twitterUsername || user?.displayName || user?.email}
-            </h1>
-            <p className="text-green-500 text-xl mb-2 sm:text-2xl sm:mb-4">
-                Click Search to find your next adventure.
-            </p>
-        <form onSubmit={handleSearch} className="mb-2 sm:mb-4">
+        <div className="absolute w-full top-[72%] transform -translate-y-1/2 flex flex-col items-center text-white px-4 md:px-0 text-xs sm:text-base">
+          <h1 className="text-lg sm:text-2xl">Welcome, {user?.twitterUsername || user?.displayName || user?.email}</h1>
+          <p className="text-green-500 text-xl mb-2 sm:text-2xl sm:mb-4">Click Search to find your next adventure.</p>
+          <form onSubmit={handleSearch} className="mb-2 sm:mb-4">
             <p className="text-sm sm:text-lg mb-1 sm:mb-2">Search for the location you want to travel to:</p>
             <div className="mb-2 sm:mb-4">
-                <input
-                    type="search"
-                    placeholder="Search here"
-                    className="border border-gray-400 rounded-lg px-3 py-2 w-full focus:outline-none text-black text-xs sm:text-base"
-                    onChange={e => {setSearchInput(e.target.value); setShowAlert(false);}}
-                    value={searchInput}
-                />
+              <input
+                type="search"
+                placeholder="Search here"
+                className="border border-gray-400 rounded-lg px-3 py-2 w-full focus:outline-none text-black text-xs sm:text-base"
+                onChange={e => {setSearchInput(e.target.value); setShowAlert(false);}}
+                value={searchInput}
+              />
             </div>
             <div className="flex flex-col sm:flex-row justify-center space-y-2 sm:space-y-0">
               <div className="flex flex-row justify-center items-center mb-2 sm:mb-0">
@@ -132,49 +127,3 @@ const HomePage: React.FC = () => {
 };
 
 export default HomePage;
-
-  // const handleSearch = async (e: React.FormEvent<HTMLFormElement>) => {
-  //   e.preventDefault();
-  
-  //   if (!searchInput || !checkInDate || !checkOutDate) {
-  //     setShowAlert(true);
-  //     setAlertMessage("Please enter location and dates before searching.");
-  //     return;
-  //   }
-  
-  //   setIsLoading(true);
-  
-  //   try {
-  //     const response = await fetch('/api/locationSearch', {
-  //       method: 'POST',
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //       },
-  //       body: JSON.stringify({
-  //         location: searchInput,
-  //         checkin: checkInDate,
-  //         checkout: checkOutDate,
-  //       }),
-  //     });
-  
-  //     if (!response.ok) { 
-  //       const text = await response.text();
-  //       throw new Error(`Server responded with status ${response.status}. Message: ${text}`);
-  //     }
-  
-  //     const data = await response.json();
-  //     console.log(data);
-  
-  //     setResults(data);
-  //     console.log("Updated Store: ", useAuthStore.getState().searchResults);
-  
-  //     navigate("/search");
-  //   } catch (error) {
-  //     console.error(error);
-  //     if (error instanceof SyntaxError) {
-  //       console.error("There was a problem parsing the response: ", error);
-  //     }
-  //   }
-  
-  //   setIsLoading(false);
-  // };
