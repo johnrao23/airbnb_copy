20b5260866e9dda38497e0359721524eb4fff6ea
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-09-11 12:38:38 -0400

    Update AboutUs.tsx

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -8,22 +8,27 @@ const About = () => {
   const user = useAuthStore((state) => state.user);
 
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="overflow-hidden min-h-screen flex flex-col">
       <NavBar />
+
       <div 
-        className="relative flex-grow bg-cover bg-no-repeat bg-center pb-5 md:pb-20 text-center md:text-right" 
+        className="flex flex-col justify-end flex-grow w-full bg-no-repeat bg-center bg-cover md:bg-150/130 pb-10 md:pb-20 text-center" 
         style={{ 
           backgroundImage: `url(${nycImg})`,
         }}
       >
-        <div className="bg-blue-500 bg-opacity-90 text-white p-3 md:p-5 rounded-lg shadow-lg mx-auto md:mx-auto mt-20 text-center max-w-xl">
-          <h1 className="text-2xl md:text-4xl">Thanks for asking about us, {user?.twitterUsername || user?.displayName || user?.email}!</h1>
-          <p className="text-lg md:text-xl">Here at Fairbnb, we want to provide you with once in a lifetime opportunities for a much better price!</p>
+        <div className="flex justify-center mt-8">
+          <div className="bg-white bg-opacity-10 p-5 md:p-10 rounded-lg shadow-lg text-center w-11/12 md:w-1/2">
+            <h1 className="text-2xl md:text-4xl font-bold mb-2 md:mb-4">Thanks for asking about us, {user?.twitterUsername || user?.displayName || user?.email}!</h1>
+            <p className="text-xl md:text-2xl mb-1 md:mb-2">Here at Fairbnb, we want to provide you with once in a lifetime opportunities for a much better price!</p>
+          </div>
         </div>
       </div>
+
       <Footer />
     </div>
   )
 }
 
 export default About;
+
