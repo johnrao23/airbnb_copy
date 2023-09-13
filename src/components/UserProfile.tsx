df63e73f5fc8ac30244c028f3558d0374891b261
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-09-13 12:38:09 -0400

    Update UserProfile.tsx

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -23,8 +23,9 @@ const Profile = () => {
     return (
         <div>
             <NavBar />
-            <div className="p-8">
-                <form>
+            <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+            { !submitted ? (
+                <form onSubmit={handleSubmit}>
                     <div className="space-y-6">
                     <div className="border-b border-gray-900/10 pb-8">
                         <h2 className="text-base font-semibold leading-7 text-gray-900">Profile</h2>
@@ -34,21 +35,27 @@ const Profile = () => {
 
                         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-6 sm:grid-cols-6">
                             <div className="sm:col-span-4">
-                                <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
-                                    Username
-                                </label>
-                                <div className="mt-2 flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                    <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">sameplacebetterprice.com/</span>
+                            <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
+                                Username
+                            </label>
+                            <div className="mt-2">
+                                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                                    <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">
+                                        sameplacebetterprice.com/
+                                    </span>
                                     <input
                                         type="text"
                                         name="username"
                                         id="username"
                                         autoComplete="username"
-                                        className="block flex-1 border-0 bg-transparent py-1 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                        value={userinfo}
+                                        onChange={handleNameChange}
+                                        className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                                         placeholder="janesmith"
                                     />
                                 </div>
                             </div>
+                        </div>
 
                             <div className="col-span-full sm:col-span-6">
                                 <label htmlFor="about" className="block text-sm font-medium leading-6 text-gray-900">
@@ -327,10 +334,13 @@ const Profile = () => {
                     </div>
                     </div>
                 </form>
+                ) : (
+                    <h2 className="text-2xl text-gray-800 mb-4">Thank you, {user?.name}</h2>
+                )}
             </div>
             <Footer />
         </div>
-    )
+    );
 }
 
 export default Profile;
