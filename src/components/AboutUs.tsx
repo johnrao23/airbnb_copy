5a271eef181bad76bc29520a0ff675c20f1fefa3
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-09-14 10:30:22 -0400

    Update AboutUs.tsx

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -21,6 +21,7 @@ const About = () => {
           <div className="bg-white bg-opacity-10 p-5 md:p-10 rounded-lg shadow-lg text-center w-11/12 md:w-1/2">
             <h1 className="text-2xl md:text-4xl font-bold mb-2 md:mb-4">Thanks for asking about us, {user?.twitterUsername || user?.displayName || user?.email}!</h1>
             <p className="text-xl md:text-2xl mb-1 md:mb-2">Here at Fairbnb, we want to provide you with once in a lifetime opportunities for a much better price!</p>
+            <p className="text-xl md:text-2xl mb-1 md:mb-2">Our idea is, to provide last minute discounted offers on rentals that vacationers would typically still have to pay full price for!</p>
           </div>
         </div>
       </div>
