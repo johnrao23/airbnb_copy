571fd3503647a9a09ff6909bc30de12e4148e6d0
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-09-15 11:39:40 -0400

    Update NavBar.tsx

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -16,7 +16,7 @@ export default function NavBar() {
 
   const navigation = [
     { name: "Home", href: "/home" },
-    { name: "Search", href: "/search" },
+    { name: "AskAi", href: "/askai" },
     { name: "About Us", href: "/about" },
     { name: "Contact", href: "/contactus" },
   ];
@@ -65,16 +65,9 @@ export default function NavBar() {
                 </div>
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <NotificationButton />
-                {/* <button
-                  onClick={NotificationButton()}
-                  type="button"
-                  className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                >
-                  <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
-                </button> */}
 
+                <NotificationButton />
+                
                 {/* Profile dropdown */}
                 <Menu as="div" className="relative ml-3">
                   <div>
