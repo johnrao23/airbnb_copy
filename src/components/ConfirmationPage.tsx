f01979a25eceff0a434faac43fbe9b417277f9da
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-09-15 11:54:48 -0400

    styling

--- src/components/ConfirmationPage.tsx
+++ src/components/ConfirmationPage.tsx
@@ -23,44 +23,44 @@ const Confirmation = () => {
     };
 
     return (
-        <>
-        <NavBar />
-        <div className="max-w-md mx-auto">
-            <h1 className="text-center text-2xl font-bold my-4">Complete Your Reservation {user?.twitterUsername || user?.displayName || user?.email}</h1>
-            <div className="w-250 h-220 overflow-hidden rounded-lg mb-2">
-                <img className="w-full h-full object-cover" src={selectedResult.images[0]} alt="Result" />
+        <div className="overflow-hidden min-h-screen flex flex-col">
+            <NavBar />
+            <div className="max-w-md mx-auto">
+                <h1 className="text-center text-2xl font-bold my-4">Complete Your Reservation {user?.twitterUsername || user?.displayName || user?.email}</h1>
+                <div className="w-250 h-220 overflow-hidden rounded-lg mb-2">
+                    <img className="w-full h-full object-cover" src={selectedResult.images[0]} alt="Result" />
+                </div>
+
+
+                <form onSubmit={handleSubmit} className="mt-8 space-y-6">
+                    <input type="hidden" name="remember" value="true" />
+                    <div className="rounded-md shadow-sm -space-y-px">
+                        <div>
+                            <label htmlFor="credit-card" className="sr-only">Credit card number</label>
+                            <input id="credit-card" name="credit-card" type="text" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Credit card number" value={creditCardNumber} onChange={e => setCreditCardNumber(e.target.value)} />
+                        </div>
+                        <div>
+                            <label htmlFor="expiry-date" className="sr-only">Expiry date</label>
+                            <input id="expiry-date" name="expiry-date" type="text" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Expiry date (MM/YY)" value={expiryDate} onChange={e => setExpiryDate(e.target.value)} />
+                        </div>
+                        <div>
+                            <label htmlFor="cvv" className="sr-only">CVV</label>
+                            <input id="cvv" name="cvv" type="text" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="CVV" value={cvv} onChange={e => setCvv(e.target.value)} />
+                        </div>
+                    </div>
+
+                    <div>
+                        <button type="submit" className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                            <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                            </span>
+                            Confirm Reservation
+                        </button>
+                    </div>
+                    <h2 className="text-red-500 font-bold text-center">***DISCLAIMER: PLEASE DO NOT ENTER ANY PERSONAL INFORMATION. THIS SITE IS FOR LEARNING PURPOSES ONLY!!!***</h2>
+                </form>
+                <Footer />
             </div>
-
-
-            <form onSubmit={handleSubmit} className="mt-8 space-y-6">
-                <input type="hidden" name="remember" value="true" />
-                <div className="rounded-md shadow-sm -space-y-px">
-                    <div>
-                        <label htmlFor="credit-card" className="sr-only">Credit card number</label>
-                        <input id="credit-card" name="credit-card" type="text" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Credit card number" value={creditCardNumber} onChange={e => setCreditCardNumber(e.target.value)} />
-                    </div>
-                    <div>
-                        <label htmlFor="expiry-date" className="sr-only">Expiry date</label>
-                        <input id="expiry-date" name="expiry-date" type="text" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Expiry date (MM/YY)" value={expiryDate} onChange={e => setExpiryDate(e.target.value)} />
-                    </div>
-                    <div>
-                        <label htmlFor="cvv" className="sr-only">CVV</label>
-                        <input id="cvv" name="cvv" type="text" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="CVV" value={cvv} onChange={e => setCvv(e.target.value)} />
-                    </div>
-                </div>
-
-                <div>
-                    <button type="submit" className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                        <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-                        </span>
-                        Confirm Reservation
-                    </button>
-                </div>
-                <h2 className="text-red-500 font-bold text-center">***DISCLAIMER: PLEASE DO NOT ENTER ANY PERSONAL INFORMATION. THIS SITE IS FOR LEARNING PURPOSES ONLY!!!***</h2>
-            </form>
-            <Footer />
         </div>
-        </>
     )
 }
 
--- src/components/ReservationPage.tsx
+++ src/components/ReservationPage.tsx
@@ -21,19 +21,19 @@ const Reservation = () => {
     return (
         <div>
             <NavBar />
-            <h1 className="text-center text-2xl font-bold my-4">Great choice {user?.twitterUsername || user?.displayName || user?.email}! Now let's make it official...</h1>
-            <div className="border border-gray-300 rounded-lg p-4 m-2 flex flex-col items-center justify-between space-y-4 h-full">
-              <h2 className="text-2xl mb-2">{selectedResult.name}</h2>
-              <div className="w-250 h-220 overflow-hidden rounded-lg mb-2">
-                  <img className="w-full h-full object-cover" src={selectedResult.images[0]} alt="Result" />
-              </div>
-              <p className="mb-2 text-green-500">${selectedResult.price.total}</p>
-              <p className="mb-2">City: {selectedResult.city}</p>
-              <p className="mb-2">Bedrooms: {selectedResult.bedrooms}</p>
-              <p className="mb-2">Bathrooms: {selectedResult.bathrooms}</p>
-              <button className="bg-blue-500 text-white rounded p-2 hover:bg-blue-700 mt-auto" onClick={() => confirmResult()}>Confirm Reservation</button>
-            </div>
-        <Footer />
+                <h1 className="text-center text-2xl font-bold my-4">Great choice {user?.twitterUsername || user?.displayName || user?.email}! Now let's make it official...</h1>
+                <div className="border border-gray-300 rounded-lg p-4 m-2 flex flex-col items-center justify-between space-y-4 h-full">
+                <h2 className="text-2xl mb-2">{selectedResult.name}</h2>
+                <div className="w-250 h-220 overflow-hidden rounded-lg mb-2">
+                    <img className="w-full h-full object-cover" src={selectedResult.images[0]} alt="Result" />
+                </div>
+                <p className="mb-2 text-green-500">${selectedResult.price.total}</p>
+                <p className="mb-2">City: {selectedResult.city}</p>
+                <p className="mb-2">Bedrooms: {selectedResult.bedrooms}</p>
+                <p className="mb-2">Bathrooms: {selectedResult.bathrooms}</p>
+                <button className="bg-blue-500 text-white rounded p-2 hover:bg-blue-700 mt-auto" onClick={() => confirmResult()}>Confirm Reservation</button>
+                </div>
+            <Footer />
         </div>
     );
 }
