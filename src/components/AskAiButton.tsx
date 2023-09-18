fc3b7ceb826c34b2fe3cefdae4607e94e142c7ed
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-09-18 10:29:59 -0400

    chat logo

--- src/components/AskAiButton.tsx
+++ src/components/AskAiButton.tsx
@@ -10,8 +10,15 @@ const AIButton = () => {
     }
 
     return (
-        <div>
-            <button onClick={handleClick}>Ask Ai</button>
+        <div className="fixed bottom-4 right-4">
+            <button 
+                onClick={handleClick} 
+                className="flex items-center justify-center px-4 py-2 bg-blue-500 text-white rounded-full hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+            >
+                {/* Imaginary ChatGPT logo. Replace <img .../> with your logo */}
+                <img src="path_to_chatgpt_logo.svg" alt="ChatGPT logo" className="w-6 h-6 mr-2" />
+                Ask Ai
+            </button>
         </div>
     )
 }
