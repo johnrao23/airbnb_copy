1cd8af64797a3bf0acac9028d31c53bb1bed0741
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-09-18 12:24:44 -0400

    Update HomePage.tsx

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -77,7 +77,7 @@ const HomePage: React.FC = () => {
         }}
       >
         <div className="absolute inset-0 bg-black opacity-40" />
-        <div className="absolute w-full top-[72%] transform -translate-y-1/2 flex flex-col items-center text-white px-4 md:px-0 text-xs sm:text-base">
+        <div className="absolute w-full top-[65%] sm:top-[72%] transform -translate-y-1/2 flex flex-col items-center text-white px-4 md:px-0 text-xs sm:text-base">
         <div className="bg-white bg-opacity-20 p-5 md:p-10 rounded-lg shadow-lg mx-auto">
           <h1 className="text-center text-lg sm:text-2xl">Welcome, {user?.twitterUsername || user?.displayName || user?.email}</h1>
           <p className="text-center font-bold text-green-500 text-xl mb-2 sm:text-2xl sm:mb-4">Click Search to find your next adventure.</p>
