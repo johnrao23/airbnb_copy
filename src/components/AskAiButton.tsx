513b3ea44e4865ac8720d7db732d8f92b5c1d7f5
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-09-18 12:22:18 -0400

    Update AskAiButton.tsx

--- src/components/AskAiButton.tsx
+++ src/components/AskAiButton.tsx
@@ -11,7 +11,7 @@ const AIButton = () => {
     }
 
     return (
-        <div className="fixed bottom-20 right-6">
+        <div className="fixed bottom-4 sm:bottom-20 right-6">
             <button 
                 onClick={handleClick} 
                 className="flex items-center justify-center bg-transparent p-0 w-16 h-16 rounded-full overflow-hidden focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 scale-100 hover:scale-105 transition-all duration-200"
