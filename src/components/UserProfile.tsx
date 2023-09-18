1d2326b56b6a3893305f1e15eaea3d1956eb5de1
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-09-18 15:45:33 -0400

    Update UserProfile.tsx

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -51,7 +51,7 @@ const Profile = () => {
                                         autoComplete="username"
                                         value={userinfo}
                                         onChange={handleNameChange}
-                                        className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                        className="block flex-1 border-0 bg-transparent py-1.5 pl-0.5 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                                         placeholder="janesmith"
                                     />
                                 </div>
