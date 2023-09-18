8b45e1f6b12c7a7e54bcd40d9baaa5e06f819672
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-09-18 10:41:34 -0400

    Update AskAiButton.tsx

--- src/components/AskAiButton.tsx
+++ src/components/AskAiButton.tsx
@@ -11,10 +11,10 @@ const AIButton = () => {
     }
 
     return (
-        <div className="fixed bottom-4 right-4">
+        <div className="fixed bottom-20 right-6">
             <button 
                 onClick={handleClick} 
-                className="flex items-center justify-center bg-transparent p-0 w-14 h-14 rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                className="flex items-center justify-center bg-transparent p-0 w-20 h-20 rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
             >
                 <img src={chatGPTlogo} alt="ChatGPT logo" className="w-full h-full rounded-full" />
             </button>
