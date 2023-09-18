0e22e08b148acf5c96ef3b0449596cae0af49987
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-09-18 11:48:17 -0400

    Update AskAiButton.tsx

--- src/components/AskAiButton.tsx
+++ src/components/AskAiButton.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import chatGPTlogo from "../assets/chatGPT.png"
+import chatGPTlogo from "../assets/chatGPT.png";
 
 const AIButton = () => {
 
@@ -14,9 +14,9 @@ const AIButton = () => {
         <div className="fixed bottom-20 right-6">
             <button 
                 onClick={handleClick} 
-                className="flex items-center justify-center bg-transparent p-0 w-16 h-16 rounded-full overflow-hidden focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                className="flex items-center justify-center bg-transparent p-0 w-16 h-16 rounded-full overflow-hidden focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 scale-100 hover:scale-105 transition-all duration-200"
             >
-                <img src={chatGPTlogo} alt="ChatGPT logo" className="w-full h-full transform scale-110 rounded-full" />
+                <img src={chatGPTlogo} alt="ChatGPT logo" className="w-full h-full transform scale-125 rounded-full" />
             </button>
         </div>
     )
