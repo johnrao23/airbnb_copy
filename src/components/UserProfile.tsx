ce893742d87bc12ff57f1945212bc27067fc8827
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-10-20 11:00:05 -0400

    Update UserProfile.tsx

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -46,6 +46,10 @@ const Profile = () => {
         setSubmitted(true);
     }
 
+    const backToProfile = () => {
+        return navigate("/profile");
+    }
+
     return (
         <div>
             <NavBar />
@@ -270,7 +274,7 @@ const Profile = () => {
                         <h2 className="text-2xl text-gray-800 mb-4">Thank you, {user?.name}</h2>
                         <button
                         type="submit"
-                        onClick={navigate("/profile")}
+                        onClick={backToProfile}
                         className="rounded-md bg-gradient-to-r from-indigo-500 to-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-gradient-to-r hover:from-indigo-400 hover:to-indigo-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 transition duration-200 ease-in-out"
                         >
                             Back to Profile
