1177fd48053d312dc265c265fac786ade4570f64
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-10-24 14:26:25 -0400

    Update UserProfile.tsx

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -79,7 +79,7 @@ const Profile = () => {
                                         id="username"
                                         autoComplete="username"
                                         value={userinfo}
-                                        onChange={handleNameChange}
+                                        onChange={handleUserID}
                                         className="block flex-1 border-0 bg-transparent py-1.5 pl-0.5 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                                         placeholder="janesmith"
                                     />
