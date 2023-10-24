50f7cc7dbf9c10ab6bca9e231e1858e5068f609e
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-10-24 14:29:22 -0400

    Update UserProfile.tsx

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -165,7 +165,7 @@ const Profile = () => {
                                     name="last-name"
                                     id="last-name"
                                     value={userinfo}
-                                    onChange={handleNameChange}
+                                    onChange={handleLastNameChange}
                                     autoComplete="family-name"
                                     className="block w-3/4 rounded-md border-0 py-1 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 />
