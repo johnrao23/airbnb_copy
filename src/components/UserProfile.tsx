1db5eba34c49768c268577c64381b76a6d547ab1
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-10-24 14:32:56 -0400

    Update UserProfile.tsx

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -149,7 +149,7 @@ const Profile = () => {
                                         type="text"
                                         name="first-name"
                                         id="first-name"
-                                        value={userinfo}
+                                        value={firstName}
                                         onChange={handleFirstNameChange}
                                         autoComplete="given-name"
                                         className="block w-3/4 rounded-md border-0 py-1 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -166,7 +166,7 @@ const Profile = () => {
                                     type="text"
                                     name="last-name"
                                     id="last-name"
-                                    value={userinfo}
+                                    value={lastName}
                                     onChange={handleLastNameChange}
                                     autoComplete="family-name"
                                     className="block w-3/4 rounded-md border-0 py-1 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
